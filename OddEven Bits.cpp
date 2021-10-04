7a7f7a5478dde53b36c4fe60ba64fb37de537df8
Md Adil Alam <31787784+Adilalaminfo@users.noreply.github.com>
2021-10-04 20:47:23 +0530

    Add files via upload

---
+++ OddEven Bits.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int a;
+  
+  cout<<"Enter the two number is "<<endl;
+  cin>>a;
+  
+ if(a&1){
+     cout<<"Number is Odd"<<endl;
+ }
+ else{
+     cout<<"Number is Even"<<endl;
+ }
+    return 0;
+}
